0cad20e2d2dad5e1defb09dd83ba087c8d707d94
alexandrapaolaorOzCO <alexandra.orozco@stu.vertexacademies.org>
2024-10-15 14:01:30 -0400

    Add files via upload

---
+++ First_Last_JS_01.js
@@ -0,0 +1,14 @@
+// This is a comment.  The // tells the computer to ignore this line.
+// Developers use comments to leave helpful notes to themselves and others.
+
+// Add your code below and click the 'Run code' button to see if it works!
+
+console.log("Hello, world!");
+
+console.log("¡Hola, Mundo!");
+
+console.log("Output:\n Hello, \n World! ");
+
+
+
+console.log("Output:\n The computer says, \"Hello, world!\"");
