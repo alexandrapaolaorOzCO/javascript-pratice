79bd6d1b6b8883904685e6f68b7f2a231c48f79d
alexandrapaolaorOzCO <alexandra.orozco@stu.vertexacademies.org>
2024-10-16 14:13:02 -0400

    Add files via upload

---
+++ Alexa_Orozco_JS_02 (1).js
@@ -0,0 +1,11 @@
+console.log(!true); 
+console.log(!!false); 
+console.log(true || true); 
+console.log(false || true); 
+console.log(true || true);
+console.log(false || true); 
+
+
+
+
+// Write 5 more expressions below:
