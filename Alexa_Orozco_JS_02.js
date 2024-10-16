34786d2695c8941891b5b818122226d6de95b07f
alexandrapaolaorOzCO <alexandra.orozco@stu.vertexacademies.org>
2024-10-16 14:10:33 -0400

    Add files via upload

---
+++ Alexa_Orozco_JS_02.js
@@ -0,0 +1,16 @@
+// Example:             // Expected Output
+console.log(2 ** 3);   // 6.5
+
+console.log(-4 * 1.5);   // -6//
+console.log(25 / 8);     // 3.125//
+console.log(2 + 3 * 5); 
+console.log((2 + 3) * 5); 
+console.log(14 % 5);
+console.log(20 % 17); 
+console.log(18 % 6); 
+
+
+
+
+
+// Write 5 more expressions below:
